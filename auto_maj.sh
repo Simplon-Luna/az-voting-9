e8f0dc48620e0d6a3ca03a6a2b5f815650500d57
Elywen <simplon.luna@gmail.com>
2023-04-25 15:06:23 +0200

    move up to date

---
+++ auto_maj.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# coding: utf-8
+# vi: tabstop=8 expandtab shiftwidth=4 softtabstop=4
+
+set -e
+
+PATCH=$(ag --nonumbers '^ver =' azure-vote/main.py | sed -re 's/ver = "1\.0\.([0-9]+)"/\1/')
+
+PATCH=$((PATCH+1))
+sed -i -re "s/(ver = \"1\.0\.).(.*)$/\1${PATCH}\2/" azure-vote/main.py
+git commit -a -m "new vers" && git push
+
+exit 0
